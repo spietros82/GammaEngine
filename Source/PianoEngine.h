204abaeaf3316f03f5fa1c302952319277b4dc76
spietros82 <spietros@gmail.com>
2026-08-25 17:10:54 +1000

    Add piano velocity-layer support

--- Source/PianoEngine.h
+++ Source/PianoEngine.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <JuceHeader.h>
+#include <array>
+#include <atomic>
 
 class PianoEngine
 {
@@ -20,7 +22,10 @@ public:
         int rootMidiNote,
         int lowMidiNote,
         int highMidiNote,
-        const juce::String& name);
+        const juce::String& name,
+        int velocityLayer = 3);
+
+    void setTouch(float touch);
 
     void noteOn(int midiNote, float velocity = 0.8f);
     void noteOff(int midiNote);
@@ -31,15 +36,23 @@ public:
         int numSamples);
 
 private:
-    juce::Synthesiser synthesiser;
+    static constexpr int numberOfVelocityLayers = 4;
+
+    std::array<juce::Synthesiser, numberOfVelocityLayers> synthesisers;
     juce::AudioFormatManager formatManager;
+
+    std::atomic<float> touchTarget { 0.5f };
+    std::array<int, 128> activeLayerForNote {};
 
     bool installReader(
         std::unique_ptr<juce::AudioFormatReader> reader,
         int rootMidiNote,
         int lowMidiNote,
         int highMidiNote,
-        const juce::String& name);
+        const juce::String& name,
+        int velocityLayer);
+
+    int chooseVelocityLayer() const;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PianoEngine)
 };
