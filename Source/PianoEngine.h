f7452df2e70c72e50403fdc9ba1358bc52abc986
spietros82 <spietros@gmail.com>
2026-08-25 16:31:53 +1000

    Add multisample zones to PianoEngine

--- Source/PianoEngine.h
+++ Source/PianoEngine.h
@@ -8,8 +8,19 @@ public:
     PianoEngine() = default;
 
     void prepare(double sampleRate);
+
+    void clearSamples();
+
     bool loadSample(const juce::File& sampleFile);
     bool loadSampleFromMemory(const void* data, size_t dataSize);
+
+    bool addSampleFromMemory(
+        const void* data,
+        size_t dataSize,
+        int rootMidiNote,
+        int lowMidiNote,
+        int highMidiNote,
+        const juce::String& name);
 
     void noteOn(int midiNote, float velocity = 0.8f);
     void noteOff(int midiNote);
@@ -23,7 +34,12 @@ private:
     juce::Synthesiser synthesiser;
     juce::AudioFormatManager formatManager;
 
-    bool installReader(std::unique_ptr<juce::AudioFormatReader> reader);
+    bool installReader(
+        std::unique_ptr<juce::AudioFormatReader> reader,
+        int rootMidiNote,
+        int lowMidiNote,
+        int highMidiNote,
+        const juce::String& name);
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(PianoEngine)
 };
